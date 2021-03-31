5034ba10ba70c585dea2aabbcbc5d993461d5914
alexsikorski <alexsikorski22@gmail.com>
2021-03-31 17:27:10 +0100

    Created tests for MergeSorter

--- src/test/java/com/sparta/alex/MergeSorterTests.java
+++ src/test/java/com/sparta/alex/MergeSorterTests.java
@@ -3,40 +3,42 @@ package com.sparta.alex;
 import com.sparta.alex.exceptions.ArrayTooSmallException;
 import com.sparta.alex.exceptions.EmptyArrayException;
 import com.sparta.alex.sorters.BubbleSorter;
+import com.sparta.alex.sorters.MergeSorter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BubbleSorterTests {
+public class MergeSorterTests {
 
-    private BubbleSorter bubbleSorter = null;
+    private MergeSorter mergeSorter = null;
 
     @BeforeEach
     void setup() {
-        bubbleSorter = new BubbleSorter();
+        mergeSorter = new MergeSorter();
     }
 
     @Test
     void sortTest() throws EmptyArrayException, ArrayTooSmallException {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, bubbleSorter.sortArray(new int[]{5, 2, 1, 4, 3}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, mergeSorter.sortArray(new int[]{5, 2, 1, 4, 3}));
     }
 
     @Test
     void nullTest() {
-        Exception exception = assertThrows(NullPointerException.class, () -> bubbleSorter.sortArray(null));
+        Exception exception = assertThrows(NullPointerException.class, () -> mergeSorter.sortArray(null));
         assertEquals("Array is null!", exception.getMessage());
     }
 
     @Test
     void emptyArrayTest() {
-        Exception exception = assertThrows(EmptyArrayException.class, () -> bubbleSorter.sortArray(new int[]{}));
+        Exception exception = assertThrows(EmptyArrayException.class, () -> mergeSorter.sortArray(new int[]{}));
         assertEquals("Empty array!", exception.getMessage());
     }
 
     @Test
-    void shortArrayTest(){
-        Exception exception = assertThrows(ArrayTooSmallException.class, () -> bubbleSorter.sortArray(new int[]{1}));
+    void shortArrayTest() {
+        Exception exception = assertThrows(ArrayTooSmallException.class, () -> mergeSorter.sortArray(new int[]{1}));
         assertEquals("Array must be larger than one!", exception.getMessage());
     }
+
 }
