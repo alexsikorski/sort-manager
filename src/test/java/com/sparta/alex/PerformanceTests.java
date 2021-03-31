f40e5470de90d84f354616fd82021180ed251b18
alexsikorski <alexsikorski22@gmail.com>
2021-03-31 18:13:21 +0100

    Implemented sort tests to analyse speed differences

--- src/test/java/com/sparta/alex/PerformanceTests.java
+++ src/test/java/com/sparta/alex/PerformanceTests.java
@@ -6,10 +6,11 @@ import com.sparta.alex.sorters.BubbleSorter;
 import com.sparta.alex.sorters.MergeSorter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.Random;
 import java.util.stream.IntStream;
-import static java.time.Duration.ofMinutes;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PerformanceTests {
 
@@ -24,17 +25,63 @@ public class PerformanceTests {
     void setup() {
         bubbleSorter = new BubbleSorter();
         mergeSorter = new MergeSorter();
-        smallSample = IntStream.generate(() -> new Random().nextInt(100)).limit(20).toArray();
-        mediumSample = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
-        largeSample = IntStream.generate(() -> new Random().nextInt(100)).limit(10000).toArray();
+        smallSample = IntStream.generate(() -> new Random().nextInt(100)).limit(1000).toArray();
+        mediumSample = IntStream.generate(() -> new Random().nextInt(100)).limit(10000).toArray();
+        largeSample = IntStream.generate(() -> new Random().nextInt(100)).limit(100000).toArray();
     }
 
     @Test
     void bubbleSortSmallSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(20, bubbleSorter.sortArray(smallSample).length);
+        assertEquals(1000, bubbleSorter.sortArray(smallSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(bubbleSorter.getClass().getName() + " || SMALL SAMPLE || time taken: " + timeTaken + "s.");
     }
+
+    @Test
+    void bubbleSortMediumSample() throws ArrayTooSmallException, EmptyArrayException {
+        long start = System.nanoTime();
+        assertEquals(10000, bubbleSorter.sortArray(mediumSample).length);
+        long end = System.nanoTime();
+        float timeTaken = (end - start) / 1000000000f;
+        System.out.println(bubbleSorter.getClass().getName() + " || MEDIUM SAMPLE || time taken: " + timeTaken + "s.");
+    }
+
+    @Test
+    void bubbleSortLargeSample() throws ArrayTooSmallException, EmptyArrayException {
+        long start = System.nanoTime();
+        assertEquals(100000, bubbleSorter.sortArray(largeSample).length);
+        long end = System.nanoTime();
+        float timeTaken = (end - start) / 1000000000f;
+        System.out.println(bubbleSorter.getClass().getName() + " || LARGE SAMPLE || time taken: " + timeTaken + "s.");
+    }
+
+    @Test
+    void mergeSortSmallSample() throws ArrayTooSmallException, EmptyArrayException {
+        long start = System.nanoTime();
+        assertEquals(1000, mergeSorter.sortArray(smallSample).length);
+        long end = System.nanoTime();
+        float timeTaken = (end - start) / 1000000000f;
+        System.out.println(mergeSorter.getClass().getName() + " || SMALL SAMPLE || time taken: " + timeTaken + "s.");
+    }
+
+    @Test
+    void mergeSortMediumSample() throws ArrayTooSmallException, EmptyArrayException {
+        long start = System.nanoTime();
+        assertEquals(10000, mergeSorter.sortArray(mediumSample).length);
+        long end = System.nanoTime();
+        float timeTaken = (end - start) / 1000000000f;
+        System.out.println(mergeSorter.getClass().getName() + " || MEDIUM SAMPLE || time taken: " + timeTaken + "s.");
+    }
+
+    @Test
+    void mergeSortLargeSample() throws ArrayTooSmallException, EmptyArrayException {
+        long start = System.nanoTime();
+        assertEquals(100000, mergeSorter.sortArray(largeSample).length);
+        long end = System.nanoTime();
+        float timeTaken = (end - start) / 1000000000f;
+        System.out.println(mergeSorter.getClass().getName() + " || LARGE SAMPLE || time taken: " + timeTaken + "s.");
+    }
+
 }
