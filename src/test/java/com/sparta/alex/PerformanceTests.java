499eb8bdde6af339879e692851dfadd749646836
alexsikorski <alexsikorski22@gmail.com>
2021-04-03 14:21:39 +0100

    Developed method that returns int array of unique integers for BinaryTree testing

--- src/test/java/com/sparta/alex/PerformanceTests.java
+++ src/test/java/com/sparta/alex/PerformanceTests.java
@@ -5,9 +5,11 @@ import com.sparta.alex.exceptions.EmptyArrayException;
 import com.sparta.alex.factory.SorterFactory;
 import com.sparta.alex.factory.SorterTypes;
 import com.sparta.alex.sorters.Sorter;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -18,10 +20,25 @@ public class PerformanceTests {
     private Sorter bubbleSorter = null;
     private Sorter mergeSorter = null;
     private Sorter binaryTreeSorter = null;
+
     private int[] smallSample = null;
     private int[] mediumSample = null;
     private int[] largeSample = null;
 
+    private static int[] uniqueSmallSample = null;
+    private static int[] uniqueMediumSample = null;
+    private static int[] uniqueLargeSample = null;
+
+    private static final int SMALL = 1000;
+    private static final int MEDIUM = 5000;
+    private static final int LARGE = 15000;
+
+    @BeforeAll
+    static void init(){
+        uniqueSmallSample = uniqueIntegerArray("small");
+        uniqueMediumSample = uniqueIntegerArray("medium");
+        uniqueLargeSample = uniqueIntegerArray("large");
+    }
 
     @BeforeEach
     void setup() {
@@ -29,15 +46,17 @@ public class PerformanceTests {
         bubbleSorter = sorterFactory.getSorter(SorterTypes.BUBBLE_SORTER);
         mergeSorter = sorterFactory.getSorter(SorterTypes.MERGE_SORTER);
         binaryTreeSorter = sorterFactory.getSorter(SorterTypes.BINARY_TREE_SORTER);
-        smallSample = IntStream.generate(() -> new Random().nextInt(100)).limit(1000).toArray();
-        mediumSample = IntStream.generate(() -> new Random().nextInt(100)).limit(10000).toArray();
-        largeSample = IntStream.generate(() -> new Random().nextInt(100)).limit(100000).toArray();
+
+        smallSample = IntStream.generate(() -> new Random().nextInt(SMALL)).limit(SMALL).toArray();
+        mediumSample = IntStream.generate(() -> new Random().nextInt(MEDIUM)).limit(MEDIUM).toArray();
+        largeSample = IntStream.generate(() -> new Random().nextInt(LARGE)).limit(LARGE).toArray();
     }
 
+    // Bubble Sorter Tests
     @Test
     void bubbleSortSmallSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(1000, bubbleSorter.sortArray(smallSample).length);
+        assertEquals(SMALL, bubbleSorter.sortArray(smallSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(bubbleSorter.getClass().getName() + " || SMALL SAMPLE || time taken: " + timeTaken + "s.");
@@ -46,7 +65,7 @@ public class PerformanceTests {
     @Test
     void bubbleSortMediumSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(10000, bubbleSorter.sortArray(mediumSample).length);
+        assertEquals(MEDIUM, bubbleSorter.sortArray(mediumSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(bubbleSorter.getClass().getName() + " || MEDIUM SAMPLE || time taken: " + timeTaken + "s.");
@@ -55,16 +74,17 @@ public class PerformanceTests {
     @Test
     void bubbleSortLargeSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(100000, bubbleSorter.sortArray(largeSample).length);
+        assertEquals(LARGE, bubbleSorter.sortArray(largeSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(bubbleSorter.getClass().getName() + " || LARGE SAMPLE || time taken: " + timeTaken + "s.");
     }
 
+    // Merge Sorter Tests
     @Test
     void mergeSortSmallSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(1000, mergeSorter.sortArray(smallSample).length);
+        assertEquals(SMALL, mergeSorter.sortArray(smallSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(mergeSorter.getClass().getName() + " || SMALL SAMPLE || time taken: " + timeTaken + "s.");
@@ -73,7 +93,7 @@ public class PerformanceTests {
     @Test
     void mergeSortMediumSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(10000, mergeSorter.sortArray(mediumSample).length);
+        assertEquals(MEDIUM, mergeSorter.sortArray(mediumSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(mergeSorter.getClass().getName() + " || MEDIUM SAMPLE || time taken: " + timeTaken + "s.");
@@ -82,16 +102,17 @@ public class PerformanceTests {
     @Test
     void mergeSortLargeSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(100000, mergeSorter.sortArray(largeSample).length);
+        assertEquals(LARGE, mergeSorter.sortArray(largeSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(mergeSorter.getClass().getName() + " || LARGE SAMPLE || time taken: " + timeTaken + "s.");
     }
 
+    // Binary Tree Sort Tests
     @Test
     void binaryTreeSortSmallSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(1000, binaryTreeSorter.sortArray(smallSample).length);
+        assertEquals(SMALL, binaryTreeSorter.sortArray(uniqueSmallSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(binaryTreeSorter.getClass().getName() + " || SMALL SAMPLE || time taken: " + timeTaken + "s.");
@@ -100,7 +121,7 @@ public class PerformanceTests {
     @Test
     void binaryTreeSortMediumSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(10000, binaryTreeSorter.sortArray(mediumSample).length);
+        assertEquals(MEDIUM, binaryTreeSorter.sortArray(uniqueMediumSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(binaryTreeSorter.getClass().getName() + " || MEDIUM SAMPLE || time taken: " + timeTaken + "s.");
@@ -109,10 +130,36 @@ public class PerformanceTests {
     @Test
     void binaryTreeLargeSample() throws ArrayTooSmallException, EmptyArrayException {
         long start = System.nanoTime();
-        assertEquals(100000, binaryTreeSorter.sortArray(largeSample).length);
+        assertEquals(LARGE, binaryTreeSorter.sortArray(uniqueLargeSample).length);
         long end = System.nanoTime();
         float timeTaken = (end - start) / 1000000000f;
         System.out.println(binaryTreeSorter.getClass().getName() + " || LARGE SAMPLE || time taken: " + timeTaken + "s.");
+    }
+
+    private static int[] uniqueIntegerArray(String sampleSize){
+        // array to be used with binary tree must have unique integers
+        HashSet<Integer> set = new HashSet<>();
+        switch (sampleSize){
+            case "small":
+                do {
+                    set.add(new Random().nextInt(SMALL));
+                }
+                while (set.size() < SMALL);
+                break;
+            case "medium":
+                do {
+                    set.add(new Random().nextInt(MEDIUM));
+                }
+                while (set.size() < MEDIUM);
+                break;
+            case "large":
+                do {
+                    set.add(new Random().nextInt(LARGE));
+                }
+                while (set.size() < LARGE);
+                break;
+        }
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 
 }
